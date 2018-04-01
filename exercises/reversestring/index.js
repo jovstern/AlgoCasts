bfdd90912d3a1ac6806163b7c2e80b1f89dd61a5
yoavsternberg <yoavsternberg@anodot.com>
2018-04-01 20:47:32 +0300

    add a third solution to reversestring/index.js

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((x, y) => y + x, '')
+}
+
+// function reverse(str) {
+//     let newStr = '';
+//     for(let char of str){
+//         newStr = char + newStr;
+//     }
+//     return newStr;
+// }
+
+
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
 
 module.exports = reverse;
